1759fe8e3084c8e796ae95347827508c48d8905e
JimboPower <edoardo.decal@gmail.com>
2019-08-26 02:08:08 +0200

    Added playgroundbook

---
+++ Edoardo.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/01.playgroundpage/LiveView.swift
@@ -0,0 +1,7 @@
+import PlaygroundSupport
+import UIKit
+
+var view = MainView(frame: UIScreen.main.bounds)
+
+PlaygroundPage.current.liveView = view
+PlaygroundPage.current.needsIndefiniteExecution = true
