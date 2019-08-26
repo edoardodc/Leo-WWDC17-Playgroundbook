a6464bfa839c423fc2751b06a0470c74bb38a1ba
JimboPower <edoardo.decal@gmail.com>
2019-08-26 02:07:23 +0200

    removed all

--- Edoardo.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/01.playgroundpage/Contents.swift
+++
@@ -1,5 +0,0 @@
-/*:
- # Leo’s adventure to find his passion
- My playground tells a story about an animal who travels the world to find his passion, which is what drives people to do incredible things. I decided to create this experience because I am a strong believer that playgrounds aren’t just great ways to learn to code, but also to tell stories. This playground relates to my very own story of finding my passion for programming. Sounds and handmade images evoke emotions to immerse the user into the story.
- */
-
--- Edoardo.playgroundbook/Contents/Chapters/Document1.playgroundchapter/Pages/01.playgroundpage/LiveView.swift
+++
@@ -1,7 +0,0 @@
-import PlaygroundSupport
-import UIKit
-
-var view = MainView(frame: UIScreen.main.bounds)
-
-PlaygroundPage.current.liveView = view
-PlaygroundPage.current.needsIndefiniteExecution = true
